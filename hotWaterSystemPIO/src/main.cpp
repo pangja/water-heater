76b8867ed36f317544318ffe89de30f5f6709357
pangja <jacktpang@gmail.com>
2023-04-08 17:00:46 +0100

    Add Comments

--- hotWaterSystemPIO/src/main.cpp
+++ hotWaterSystemPIO/src/main.cpp
@@ -6,5 +6,16 @@ void setup() {
 }
 
 void loop() {
+
   // put your main code here, to run repeatedly:
+  // modes: [1] off, [2] on, [3] cooldown, [4] fill 
+
+  // case [1]: pump off, diverter position 1 (hot air), ball valve closed
+  // case [2]: pump off/on, diverter position 2 (hot water), ball valve closed
+  // case [3]: pump on, diverter position 1, ball valve closed
+  // case [4]: pump off, diveter position (hot air), ball valve open
+
+  // if (tankEmpty == false) {
+  //}
+
 }
